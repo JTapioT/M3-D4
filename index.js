32da9986837ae315beb1912397976ba6b025c03b
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-09-17 08:57:29 +0200

    Update to homework
    Little update to yesterday homework. Now cart is modal, which get list of books added. Continue later.

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 window.onload = () => {
 
+  let shoppingCart = document.querySelector(".shoppingCart");
+
+
   let booksContainer = document.querySelector(".booksContainer");
-  let cartContainer = document.querySelector(".cartContainer");
+  
 
   function listBooks(booksList) {
     // EXERCISE 2 - .forEach:
-    booksList.forEach((book) => {
+    booksList.forEach((book, index) => {
       let colDivElement = document.createElement("div");
-      colDivElement.classList.add("col-4");
+      colDivElement.classList.add("col-3");
+      colDivElement.classList.add("mb-3");
 
       let cardDivElement = document.createElement("div");
       cardDivElement.classList.add("card");
+      cardDivElement.id = index;
 
       cardDivElement.innerHTML = `
-        <img src=${book.img} class="" alt="..." style="width: 100%; object-fit:cover;>
-        <div class="card-body">
-        <h6 class="card-title mt-2">${book.title}</h6>
-        <p class="card-text">${book.price}$</p>
-        <a href="#" class="btn btn-primary btn-cart">ADD TO CART</a>
-        <a href="#" class="btn btn-warning">Skip this book</a>`;
+        <div style="height: 300px;">
+        <img src=${book.img} class="img-fluid" style="width: 100%; height: 100%">
+        </div>
+        <div class="d-flex flex-column px-1 justify-content-between">
+        <h6 class="card-title m-0 mt-2">${book.title}</h6>
+        <p class="card-text m-0">${book.price}$</p>
+        <div>
+        <a href="#" class="btn btn-primary btn-cart w-100">ADD TO CART</a>
+        <a href="#" class="btn btn-warning mt-2 w-100">Skip this book</a>
+        </div>`;
 
       colDivElement.appendChild(cardDivElement);
       booksContainer.appendChild(colDivElement);
@@ -40,29 +49,45 @@ window.onload = () => {
     let addToCartButtonArray = Array.from(addToCartButtonNodeList);
     addToCartButtonArray.forEach((button) => {
       button.addEventListener("click", (event) => {
-        // Make content such as title and cards visible:
-        // Rename better later the contentContainer...
-        let cartContentContainer = document.querySelector(".cartContent");
-        cartContentContainer.classList.remove("d-none");
+        // Change shopping cart image:
+        let shoppingCart = document.querySelector(".shoppingCart");
+        shoppingCart.src = "./assets/fullcart.png";
 
-        let colDivElement = document.createElement("div");
-        colDivElement.classList.add("col-4");
-
-        let cardElement = event.target.closest(".card");
+        // Add information to the shopping cart body:
+        let bookId = event.target.closest(".card").id;
         
-        let deleteBtn = document.createElement("a");
-        deleteBtn.className = "btn btn-success"
-        deleteBtn.innerText = "Remove from cart";
+        let bookInfo = document.createElement("div");
+        bookInfo.id = bookId;
+        bookInfo.classList.add("cartCard");
+        bookInfo.className = "d-flex justify-content-between align-items-center mb-3";
+        bookInfo.innerHTML = `
+          <img src=${booksList[bookId].img} class="img-fluid" style="width: 60px; object-fit: cover;">
+          <h6 class="card-title m-0 p-0" style=" display: -webkit-box;-webkit-line-clamp:1 ;-webkit-box-orient: vertical; overflow: hidden;">${booksList[bookId].title}</h6>
+          <p class="card-text m-0 p-0">${booksList[bookId].price}$</p>
+          <i class="bi bi-exclamation-octagon removeFromCart" style="font-size: 40px; color:red; cursor: pointer;"></i>
+        `;
+        let cartContainer = document.querySelector(".cartContainer");
+        cartContainer.appendChild(bookInfo);
 
-        cardElement.appendChild(deleteBtn);
+        // HANDLER FOR REMOVAL FROM CART MODAL:
 
-        colDivElement.appendChild(cardElement);
-
-        cartContainer.appendChild(colDivElement);
-
-        cardElement.style.background = "grey";
+      shoppingCart.addEventListener("click", () => {
+        
+        let removeFromCartIcon = document.querySelectorAll(".removeFromCart");
+        removeFromCartIcon.forEach(removeIcon => {
+          console.log(removeIcon);
+          removeIcon.addEventListener("click", (event) => {
+            console.log(event.target.closest(".cartCard"));
+          })
+        })
       })
-    })
+        
+
+      });
+
+    });
+
+
   }
 
   // EXERCISE 1:
